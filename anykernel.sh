465f39031cb18057f912ad2a90a1177482c63756
Danny Lin <danny@kdrag0n.dev>
2021-03-14 13:46:07 +0000

    ak3: Skip ramdisk unpacking and repacking
    We don't currently modify anything in the ramdisk, so skip these steps
    to speed up the flashing process.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    Signed-off-by: reaPeR1010 <reaPeR10x10x@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -27,8 +27,9 @@ patch_vbmeta_flag=auto;
 . tools/ak3-core.sh;
 
 # boot install
-dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+split_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
 
-write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+flash_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+flash_dtbo;
 ## end boot install
 
