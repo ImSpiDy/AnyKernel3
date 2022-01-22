daa7bd75c6062a99a4b0b9e5ed8c6b1fbf893859
Alexander Winkowski <dereference23@outlook.com>
2022-01-22 16:47:18 +0000

    ak3: Hide block device path

--- anykernel.sh
+++ anykernel.sh
@@ -24,6 +24,7 @@ BLOCK=/dev/block/bootdevice/by-name/boot;
 IS_SLOT_DEVICE=auto;
 RAMDISK_COMPRESSION=auto;
 PATCH_VBMETA_FLAG=auto;
+NO_BLOCK_DISPLAY=1;
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
