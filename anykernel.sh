f93ba7b9d8ff9bba7cee58c4c3117b0f22feb84d
ImSpiDy <spidy2713@gmail.com>
2022-02-01 12:21:22 +0000

    Anykernel: update Name
    Signed-off-by: ImSpiDy <spidy2713@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=aRiseKernel By Prashant
+kernel.string=Flashing Xtreme Kernel ......
 do.devicecheck=1
 do.modules=0
 do.systemless=1
