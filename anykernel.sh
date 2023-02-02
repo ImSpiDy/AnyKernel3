940127b70387be57ac53f4a39e4c2f18f6b6abfa
kdrag0n <danny@kdrag0n.dev>
2023-02-02 14:09:31 +0000

    ak3: Add detected Android version to kernel cmdline
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -26,6 +26,13 @@ patch_vbmeta_flag=auto;
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
 
+ver="$(file_getprop /system/build.prop ro.build.version.release)"
+if [ ! -z "$ver" ]; then
+  patch_cmdline "androidboot.version" "androidboot.version=$ver"
+else
+  patch_cmdline "androidboot.version" ""
+fi
+
 # boot install
 split_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
 
